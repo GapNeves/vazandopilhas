1fd0b0ef92bf1db71ff745050ea912a2cfc768ff
GapNeves <gabriel.neves84@hotmail.com>
2024-03-08 22:23:55 -0300

    chore: Adding input in forms

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import Form from "./components/Form";
 function App() {
   return (
     <div>
-      <Form />
+      <h1>
+        Cadastre-se
+      </h1>
+      <p>
+        Cadastre-se para tirar suas duvidas.
+      </p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Nessa comunidade você tera liberdade de fazer perguntas e poder receber
+        respostas de pessoas voluntarias que te ajudaram em suas duvidas.
+      </p>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,38 @@
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+
+      <div>
+        <label htmlFor="name">
+          Nome
+        </label>
+        <input type="text" placeholder="Digite o seu noem" />
+      </div>
+
+      <div>
+        <label htmlFor="email">
+          E-mail
+        </label>
+        <input type="email" placeholder="Insira seu email"/>
+      </div>
+
+      <div>
+        <a href="#">
+          Leia os termos
+        </a>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="agree">
+            Concordo com os termos.
+          </label>
+        </div>
+      </div>
+
+      <button>
+        Cadastrar
+      </button>
+
+    </form>
   );
 };
 
